3ceff8b09c351bd5654911153f9bec4671ae3920
Your NameTamir <tamirn7@gmail.com>
2020-06-09 09:49:38 +0300

    nothing changed

--- api/Socket/SocketRoutes.js
+++ api/Socket/SocketRoutes.js
@@ -8,10 +8,10 @@ const UtilService = require('../../services/UtilService')
 
 function connectSockets(io) {
     io.on('connection', socket => {
-        socket.on('Add Friend', ({ friendId, _id, type, userName, fullName, imgUrl }) => {            
+        socket.on('Add Friend', ({ friendId, _id, type, userName, fullName, imgUrl }) => {
             const notification = {
-                _id:ObjectId(UtilService.makeId()),
-                userId:ObjectId(_id),
+                _id: ObjectId(UtilService.makeId()),
+                userId: ObjectId(_id),
                 // roomId:ObjectId(UtilService.makeId()),
                 createdAt: Date.now(),
                 userName,
@@ -24,68 +24,69 @@ function connectSockets(io) {
                 .then(async user => {
                     console.log(user);
                     user.notifications.push(notification)
-                    const updatedUser = await userService.update(user, true)                    
+                    const updatedUser = await userService.update(user, true)
                     io.emit(`updateUser ${friendId}`, updatedUser)
                 })
         })
         socket.on('decline', notification => {
             userService.getById(notification.userId)
                 .then(async user => {
-        //             user.notifications.push(notification)
-        //             const updatedUser = await userService.update(user, true)
-        //             io.emit(`updatedUser ${user._id}`, updatedUser)
-        //         })
-        //     userService.getById(notification.from._id)
-        //         .then(async user => {
-        //             const idx = user.notifications.findIndex(
-        //                 currProj => currProj._id === notification._id
-        //               );
-        //             user.notifications.splice(idx, 1);
-        //             await userService.update(user, true)
+                    //             user.notifications.push(notification)
+                    //             const updatedUser = await userService.update(user, true)
+                    //             io.emit(`updatedUser ${user._id}`, updatedUser)
+                    //         })
+                    //     userService.getById(notification.from._id)
+                    //         .then(async user => {
+                    //             const idx = user.notifications.findIndex(
+                    //                 currProj => currProj._id === notification._id
+                    //               );
+                    //             user.notifications.splice(idx, 1);
+                    //             await userService.update(user, true)
                 })
         })
-        socket.on('approve', async ({notification, _id}) => {  
+        socket.on('approve', async({ notification, _id }) => {
+            const reciveingUser = await userService.getById(_id)
+            const roomId = ObjectId(UtilService.makeId())
+            reciveingUser.friends.push({
+                roomId,
+                _id: ObjectId(notification.userId),
+                userName: notification.userName,
+                fullName: notification.fullName,
+                imgUrl: notification.imgUrl,
+            })
+            const idx = reciveingUser.notifications.findIndex(
+                currNotification => currNotification._id === notification._id
+            );
+            reciveingUser.notifications.splice(idx, 1);
+            // reciveingUser.notifications.push({
+
+            // })
+
+            userService.update(reciveingUser, true)
+
+            const sendingUser = await userService.getById(notification.userId)
+
             
-            const reciveingUser = await userService.getById(_id)               
-            
-                  const roomId = ObjectId(UtilService.makeId())
-                  reciveingUser.friends.push({
-                      roomId,
-                 _id:ObjectId(notification.userId),
-                 userName:notification.userName,
-                 fullName:notification.fullName,
-                 imgUrl:notification.imgUrl,
-                  })
-                  const idx = reciveingUser.notifications.findIndex(
-                        currNotification => currNotification._id === notification._id
-                      );
-                      reciveingUser.notifications.splice(idx, 1);
-                userService.update(reciveingUser, true)
-            
-                const sendingUser = await userService.getById(notification.userId)
-                
-                //    
-                // user.notifications.push({})
-                sendingUser.friends.push({
-                    roomId,
-                    _id:ObjectId(_id),
-                    userName:reciveingUser.userName,
-                    fullName:reciveingUser.fullName,
-                    imgUrl:reciveingUser.imgUrl,
-                })
-                userService.update(sendingUser, true)
-                console.log('notification',sendingUser);
-                
-                
+            sendingUser.friends.push({
+                roomId,
+                _id: ObjectId(_id),
+                userName: reciveingUser.userName,
+                fullName: reciveingUser.fullName,
+                imgUrl: reciveingUser.imgUrl,
+            })
+            userService.update(sendingUser, true)
+            console.log('notification', sendingUser);
+
+
             //  })
-             
+
             //         await projService.update(proj);
             //         const idx = user.notifications.findIndex(
             //             currProj => currProj._id === notification._id
             //         );
             //         user.notifications.splice(idx, 1);
             //         await userService.update(user, true)
-                // })
-        })  
+            // })
+        })
     })
 }
