4eca25d71e1a52c7f5c73d957dc19c965b434804
Your NameTamir <tamirn7@gmail.com>
2020-10-18 13:24:24 +0300

    push to pull

--- api/room/room.service.js
+++ api/room/room.service.js
@@ -1,164 +1,170 @@
-
-const dbService = require('../../services/DbService')
-const ObjectId = require('mongodb').ObjectId
+const dbService = require("../../services/DbService");
+const ObjectId = require("mongodb").ObjectId;
 
 module.exports = {
-    query,
-    getById,
-    remove,
-    update,
-    add,
-    checkIsValidUser,
-    removeNote,
-    changeNoteColor,
-    toggleNotePin,
-    updateNote
-}
+  query,
+  getById,
+  remove,
+  update,
+  add,
+  checkIsValidUser,
+  removeNote,
+  changeNoteColor,
+  toggleNotePin,
+  updateNote,
+};
 
 async function query(filterBy = {}) {
-    const critirea = _buildCriteria(filterBy);
-    const collection = await dbService.getCollection('room');
-    try {
-        const rooms = await collection.find(critirea.query).sort(critirea.sortBy).toArray();
-        return rooms;
-    } catch (err) {
-        console.log('ERROR: cannot find Rooms')
-        throw err;
-    }
+  const critirea = _buildCriteria(filterBy);
+  const collection = await dbService.getCollection("room");
+  try {
+    const rooms = await collection
+      .find(critirea.query)
+      .sort(critirea.sortBy)
+      .toArray();
+    return rooms;
+  } catch (err) {
+    console.log("ERROR: cannot find Rooms");
+    throw err;
+  }
 }
 
 async function getById(filterBy) {
-    const collection = await dbService.getCollection('room')
-    try {
-        const room = await collection.findOne({ "_id": ObjectId(filterBy.roomId) })
-        if (filterBy.term) {
-            let term = filterBy.term.toLowerCase()
-            room.notes = room.notes.filter(note => {
-                if (note.header.includes(filterBy.term)) return note
-                else switch (note.type) {
-                    case 'NoteLoc':
-                        return note.data.name.toLowerCase().includes(term)
-                    case 'NoteTodo':
-                        return note.data.some(todo => todo.text.toLowerCase().includes(term));
-                    default:
-                        return note.data.toLowerCase().includes(term)
-                }
-            })
-        }
-       room.notes = (filterBy.type) ? room.notes.filter(note => note.type === filterBy.type) : room.notes
-        return room
-    } catch (err) {
-        console.log(`ERROR: while finding room ${filterBy.roomId}`)
-        throw err;
+  const collection = await dbService.getCollection("room");
+  try {
+    const room = await collection.findOne({ _id: ObjectId(filterBy.roomId) });
+    if (filterBy.term) {
+      let term = filterBy.term.toLowerCase();
+      room.notes = room.notes.filter((note) => {
+        if (note.header.includes(filterBy.term)) return note;
+        else
+          switch (note.type) {
+            case "NoteLoc":
+              return note.data.name.toLowerCase().includes(term);
+            case "NoteTodo":
+              return note.data.some((todo) =>
+                todo.text.toLowerCase().includes(term),
+              );
+            default:
+              return note.data.toLowerCase().includes(term);
+          }
+      });
     }
+    room.notes = filterBy.type
+      ? room.notes.filter((note) => note.type === filterBy.type)
+      : room.notes;
+    return room;
+  } catch (err) {
+    console.log(`ERROR: while finding room ${filterBy.roomId}`);
+    throw err;
+  }
 }
 
-
 async function remove(roomId) {
-    const collection = await dbService.getCollection('room')
-    try {
-        await collection.deleteOne({ "_id": ObjectId(roomId) })
-    } catch (err) {
-        console.log(`ERROR: cannot remove room ${roomId}`)
-        throw err;
-    }
+  const collection = await dbService.getCollection("room");
+  try {
+    await collection.deleteOne({ _id: ObjectId(roomId) });
+  } catch (err) {
+    console.log(`ERROR: cannot remove room ${roomId}`);
+    throw err;
+  }
 }
 
 async function update(room) {
-    const collection = await dbService.getCollection('room')
-    room._id = ObjectId(room._id);
-    try {
-        await collection.replaceOne({ "_id": room._id }, { $set: room })
-        return room;
-    } catch (err) {
-        console.log(`ERROR: cannot update room ${room._id}`)
-        throw err;
-    }
+  const collection = await dbService.getCollection("room");
+  room._id = ObjectId(room._id);
+  try {
+    await collection.replaceOne({ _id: room._id }, { $set: room });
+    return room;
+  } catch (err) {
+    console.log(`ERROR: cannot update room ${room._id}`);
+    throw err;
+  }
 }
 
 async function add(room) {
-    room.createdAt = Date.now();
-    const collection = await dbService.getCollection('room');
-    try {
-        await collection.insertOne(room);
-        console.log('backend room returned', room);
-        return room;
-    } catch (err) {
-        console.log(`ERROR: cannot insert room`)
-        throw err;
-    }
+  room.createdAt = Date.now();
+  const collection = await dbService.getCollection("room");
+  try {
+    await collection.insertOne(room);
+    console.log("backend room returned", room);
+    return room;
+  } catch (err) {
+    console.log(`ERROR: cannot insert room`);
+    throw err;
+  }
 }
 
 async function checkIsValidUser(userId, roomId) {
-    console.log('**roomID**', room);
-    const room = await getById({ roomId })
-    console.log('**room***', room);
-    return room.members.some(memberId => memberId.toString() === userId)
+  console.log("**roomID**", room);
+  const room = await getById({ roomId });
+  console.log("**room***", room);
+  return room.members.some((memberId) => memberId.toString() === userId);
 }
 
 async function removeNote(roomId, noteId) {
-    const room = await getById({ roomId })
-    const idx = room.notes.findIndex(note => note._id === noteId)
-    room.notes.splice(idx, 1)
-    const updatedRoom = await update(room)
-    return updatedRoom
+  const room = await getById({ roomId });
+  const idx = room.notes.findIndex((note) => note._id === noteId);
+  room.notes.splice(idx, 1);
+  const updatedRoom = await update(room);
+  return updatedRoom;
 }
 
 async function changeNoteColor(roomId, noteId, color) {
-    const room = await getById({ roomId })
-    const idx = room.notes.findIndex(note => note._id === noteId)
-    room.notes[idx].bgColor = color
-    const updatedRoom = await update(room)
-    return updatedRoom
+  const room = await getById({ roomId });
+  const idx = room.notes.findIndex((note) => note._id === noteId);
+  room.notes[idx].bgColor = color;
+  const updatedRoom = await update(room);
+  return updatedRoom;
 }
 
 async function toggleNotePin(roomId, noteId) {
-    let room = await getById({ roomId })
-    const idx = room.notes.findIndex(note => note._id === noteId)
-    const note = room.notes.splice(idx, 1)[0] //splice defalt behavior is return array
-    note.isPinned = !note.isPinned
-    room = (note.isPinned) ? _handleNotePin(room, note) : _handleNoteUnpin(room, note)
-    const updatedRoom = await update(room)
-    return updatedRoom
+  let room = await getById({ roomId });
+  const idx = room.notes.findIndex((note) => note._id === noteId);
+  const note = room.notes.splice(idx, 1)[0]; //splice defalt behavior is return array
+  note.isPinned = !note.isPinned;
+  room = note.isPinned
+    ? _handleNotePin(room, note)
+    : _handleNoteUnpin(room, note);
+  const updatedRoom = await update(room);
+  return updatedRoom;
 }
-
 
 async function updateNote(roomId, note) {
-    const room = await getById({ roomId })
-    const idx = room.notes.findIndex(currNote => note._id === currNote._Id)
-    room.notes.splice(idx, 1, note)
-    const updatedRoom = await update(room)
-    return updatedRoom
+  console.log("note", note);
+  const room = await getById({ roomId });
+  const idx = room.notes.findIndex((currNote) => currNote._id === note._id);
+  room.notes.splice(idx, 1, note);
+  const updatedRoom = await update(room);
+  return updatedRoom;
 }
 
-
 function _handleNotePin(room, note) {
-    room.notes.unshift(note)
-    return room
+  room.notes.unshift(note);
+  return room;
 }
 
 function _handleNoteUnpin(room, note) {
-    let idx = room.notes.findIndex(currNote => (!currNote.isPinned && currNote.createdAt <= note.createdAt))
-    idx === -1 ? room.notes.push(note) : room.notes.splice(idx, 0, note)
-    return room
+  let idx = room.notes.findIndex(
+    (currNote) => !currNote.isPinned && currNote.createdAt <= note.createdAt,
+  );
+  idx === -1 ? room.notes.push(note) : room.notes.splice(idx, 0, note);
+  return room;
 }
-
 
 function _buildCriteria(filterBy) {
-    const critirea = {
-        query: {},
-        sortBy: {}
-    };
-    // if (filterBy.boardId) {
-    //     if (filterBy.searchIn === 'genres') critirea.query.boardId = { $in: [filterBy.boardId] };
-    // else critirea.query.title = { $regex: filterBy.txt, $options: 'i' };
-    // } 
-    if (filterBy.sortBy === 'date') critirea.sortBy.createdAt = -1;
-    else critirea.sortBy.title = 1;
-    return critirea;
+  const critirea = {
+    query: {},
+    sortBy: {},
+  };
+  // if (filterBy.boardId) {
+  //     if (filterBy.searchIn === 'genres') critirea.query.boardId = { $in: [filterBy.boardId] };
+  // else critirea.query.title = { $regex: filterBy.txt, $options: 'i' };
+  // }
+  if (filterBy.sortBy === "date") critirea.sortBy.createdAt = -1;
+  else critirea.sortBy.title = 1;
+  return critirea;
 }
-
-
 
 // function _buildCriteria(filterBy) {
 //     const critirea = {
@@ -168,16 +174,15 @@ function _buildCriteria(filterBy) {
 //     if (filterBy.txt) {
 //         if (filterBy.searchIn === 'genres') critirea.query.labels = { $in: [filterBy.txt] };
 //         else critirea.query.title = { $regex: filterBy.txt, $options: 'i' };
-//     } 
+//     }
 
-//     if (filterBy.sortBy === 'date') critirea.sortBy.createdAt = -1; 
+//     if (filterBy.sortBy === 'date') critirea.sortBy.createdAt = -1;
 //     else critirea.sortBy.title = 1;
 
 //     return critirea;
 // }
 
-
-// NOTE JSON MODEL: 
+// NOTE JSON MODEL:
 // {
 //     "header":"3",
 //     "data":"3",
